27fdc872c655d627cfc2ba3fd60c807b128abf65
Kangning Li <likangning93@gmail.com>
2018-05-30 11:26:32 -0400

    cleanup?

--- Source/Shaders/PolylineShadowVolumeVS.glsl
+++ Source/Shaders/PolylineShadowVolumeVS.glsl
@@ -29,85 +29,74 @@ varying vec4 v_color;
 varying vec2 v_alignedPlaneDistances;
 #endif
 
-vec3 morphVector(vec3 startNorm, vec4 startPos, vec3 endNorm, vec4 endPos, float t) {
-    vec3 startNormPos = startNorm + startPos.xyz;
-    vec3 endNormPos = endNorm + endPos.xyz;
+// https://keithmaggio.wordpress.com/2011/02/15/math-magician-lerp-slerp-and-nlerp/
+vec3 slerp(vec3 start, vec3 end, float percent)
+{
+     // Dot product - the cosine of the angle between 2 vectors.
+     float dotProduct = dot(start, end);
+     // Clamp it to be in the range of Acos()
+     // This may be unnecessary, but floating point
+     // precision can be a fickle mistress.
+     clamp(dotProduct, -1.0, 1.0);
+     // Acos(dot) returns the angle between start and end,
+     // And multiplying that by percent returns the angle between
+     // start and the final result.
+     float theta = acos(dotProduct)*percent;
+     vec3 relativeVec = normalize(end - start*dotProduct); // Orthonormal basis
+     // The final result.
+     return ((start*cos(theta)) + (relativeVec*sin(theta)));
+}
 
-    vec3 medNormPos = mix(startNormPos, endNormPos, t);
-    vec3 medPos = mix(startPos.xyz, endPos.xyz, t);
-    //return normalize(medNormPos - medPos);
-    return normalize(mix(startNorm, endNorm, t));
+vec3 morphNormal(vec3 norm2D, vec4 pos2D, vec3 norm3D, vec4 pos3D) {
+    vec3 normPos2D = 100000.0 * norm2D + pos2D.xyz;
+    vec3 normPos3D = 100000.0 * norm3D + pos3D.xyz;
+
+    vec3 normPos = mix(normPos2D, normPos3D, czm_morphTime);
+    vec3 pos = mix(pos2D.xyz, pos3D.xyz, czm_morphTime);
+    return normalize(normPos - pos); // bad for 3D unless normals padded out a LOT, doesn't work when morphing, ok for 2D?
+    //return normalize(slerp(norm2D, norm3D, czm_morphTime)); // doesn't produce great results for 3D, doesn't work when morphing
+    //return normalize(mix(norm2D, norm3D, czm_morphTime)); // pure 2D and 3D work fine, but morph doesn't work
 }
 
 void main()
 {
-//#ifdef COLUMBUS_VIEW_2D
-    vec4 ecStart2D = czm_modelViewRelativeToEye * czm_translateRelativeToEye(vec3(0.0, startHiLo2D.xy), vec3(0.0, startHiLo2D.zw));
+    // Start and End positions
+    vec4 startRelativeToEye2D = czm_translateRelativeToEye(vec3(0.0, startHiLo2D.xy), vec3(0.0, startHiLo2D.zw));
+    vec4 startRelativeToEye3D = czm_translateRelativeToEye(startHi_and_forwardOffsetX.xyz, startLo_and_forwardOffsetY.xyz);
+    vec4 startRelativeToEye = czm_columbusViewMorph(startRelativeToEye2D, startRelativeToEye3D, czm_morphTime);
+    vec3 ecStart = (czm_modelViewRelativeToEye * startRelativeToEye).xyz;
 
-    vec3 forwardDirectionEC2D = czm_normal * vec3(0.0, offsetAndRight2D.xy);
-    vec4 ecEnd2D = vec4(forwardDirectionEC2D, 0.0) + ecStart2D;
-    forwardDirectionEC2D = normalize(forwardDirectionEC2D);
+    vec4 endRelativeToEye2D = startRelativeToEye2D + vec4(0.0, offsetAndRight2D.xy, 0.0);
+    vec4 endRelativeToEye3D = startRelativeToEye3D + vec4(startHi_and_forwardOffsetX.w, startLo_and_forwardOffsetY.w, startNormal_and_forwardOffsetZ.w, 0.0);
+    vec4 endRelativeToEye = czm_columbusViewMorph(endRelativeToEye2D, endRelativeToEye3D, czm_morphTime);
+    vec3 ecEnd = (czm_modelViewRelativeToEye * endRelativeToEye).xyz;
 
-    // Right plane
-    vec4 rightPlaneEC2D;
-    rightPlaneEC2D.xyz = czm_normal * vec3(0.0, offsetAndRight2D.zw);
-    //rightPlaneEC2D.w = -dot(rightPlaneEC2D.xyz, ecStart2D);
+    // Start plane
+    vec3 startPlaneDir2D = vec3(0.0, startEndNormals2D.xy);
+    vec3 startPlaneDir3D = startNormal_and_forwardOffsetZ.xyz;
+    vec3 startPlaneDirEC = czm_normal * morphNormal(startPlaneDir2D, startRelativeToEye2D, startPlaneDir3D, startRelativeToEye3D);
+    v_startPlaneEC.xyz = startPlaneDirEC;
+    v_startPlaneEC.w = -dot(startPlaneDirEC, ecStart);
 
-    // start plane
-    vec4 startPlaneEC2D;
-    startPlaneEC2D.xyz =  czm_normal * vec3(0.0, startEndNormals2D.xy);
-    //startPlaneEC2D.w = -dot(startPlaneEC2D.xyz, ecStart2D);
-
-    // end plane
-    vec4 endPlaneEC2D;
-    endPlaneEC2D.xyz =  czm_normal * vec3(0.0, startEndNormals2D.zw);
-    //endPlaneEC2D.w = -dot(endPlaneEC2D.xyz, ecEnd2D);
-
-    vec3 texcoordNormalization_and_halfWidth2D;
-    texcoordNormalization_and_halfWidth2D.xy = texcoordNormalization2D;
-
-//#else // COLUMBUS_VIEW_2D
-    vec4 ecStart3D = czm_modelViewRelativeToEye * czm_translateRelativeToEye(startHi_and_forwardOffsetX.xyz, startLo_and_forwardOffsetY.xyz);
-    vec3 offset3D = czm_normal * vec3(startHi_and_forwardOffsetX.w, startLo_and_forwardOffsetY.w, startNormal_and_forwardOffsetZ.w);
-    vec4 ecEnd3D = ecStart3D + vec4(offset3D, 0.0);
-
-    vec3 forwardDirectionEC3D = normalize(offset3D);
-
-    // start plane
-    vec4 startPlaneEC3D;
-    startPlaneEC3D.xyz = czm_normal * startNormal_and_forwardOffsetZ.xyz;
-    //startPlaneEC3D.w = -dot(startPlaneEC3D.xyz, ecStart3D);
-
-    // end plane
-    vec4 endPlaneEC3D;
-    endPlaneEC3D.xyz = czm_normal * endNormal_andTextureCoordinateNormalizationX.xyz;
-    //endPlaneEC3D.w = -dot(endPlaneEC3D.xyz, ecEnd3D);
+    // End plane
+    vec3 endPlaneDir2D = vec3(0.0, startEndNormals2D.zw);
+    vec3 endPlaneDir3D = endNormal_andTextureCoordinateNormalizationX.xyz;
+    vec3 endPlaneDirEC = czm_normal * morphNormal(endPlaneDir2D, endRelativeToEye2D, endPlaneDir3D, endRelativeToEye3D);
+    v_endPlaneEC.xyz = endPlaneDirEC;
+    v_endPlaneEC.w = -dot(endPlaneDirEC, ecEnd);
 
     // Right plane
-    vec4 rightPlaneEC3D;
-    rightPlaneEC3D.xyz = czm_normal * rightNormal_andTextureCoordinateNormalizationY.xyz;
-    //rightPlaneEC3D.w = -dot(rightPlaneEC3D.xyz, ecStart3D);
+    vec3 right2D = vec3(0.0, offsetAndRight2D.zw);
+    vec3 right3D = rightNormal_andTextureCoordinateNormalizationY.xyz;
+    vec3 rightEC = czm_normal * morphNormal(right2D, startRelativeToEye2D, right3D, startRelativeToEye3D);
+    v_rightPlaneEC.xyz = rightEC;
+    v_rightPlaneEC.w = -dot(rightEC, ecStart);
 
-    vec3 texcoordNormalization_and_halfWidth3D;
-    texcoordNormalization_and_halfWidth3D.xy = vec2(endNormal_andTextureCoordinateNormalizationX.w, rightNormal_andTextureCoordinateNormalizationY.w);
+    // Forward direction
+    vec3 forwardDirectionEC = normalize(ecEnd - ecStart);
+    v_forwardDirectionEC = forwardDirectionEC;
 
-//#endif // COLUMBUS_VIEW_2D
-
-    vec4 p = mix(ecStart2D, ecStart3D, czm_morphTime);
-
-    vec3 ecStart = p.xyz;
-    vec3 ecEnd = ecStart + mix(czm_normal * vec3(0.0, offsetAndRight2D.xy), offset3D, czm_morphTime);
-
-    v_startPlaneEC.xyz = morphVector(startPlaneEC2D.xyz, ecStart2D, startPlaneEC3D.xyz, ecStart3D, czm_morphTime);
-    v_endPlaneEC.xyz = morphVector(endPlaneEC2D.xyz, ecEnd2D, endPlaneEC3D.xyz, ecEnd3D, czm_morphTime);
-    v_rightPlaneEC.xyz = morphVector(rightPlaneEC2D.xyz, ecStart2D, rightPlaneEC3D.xyz, ecStart3D, czm_morphTime);
-
-    v_startPlaneEC.w = -dot(v_startPlaneEC.xyz, ecStart);
-    v_endPlaneEC.w = -dot(v_endPlaneEC.xyz, ecEnd);
-    v_rightPlaneEC.w = -dot(v_rightPlaneEC.xyz, ecStart);
-
-    v_forwardDirectionEC = morphVector(forwardDirectionEC2D, ecStart2D, forwardDirectionEC3D, ecStart3D, czm_morphTime);
-    v_texcoordNormalization_and_halfWidth = mix(texcoordNormalization_and_halfWidth2D, texcoordNormalization_and_halfWidth3D, czm_morphTime);
+    v_texcoordNormalization_and_halfWidth.xy = mix(texcoordNormalization2D, vec2(endNormal_andTextureCoordinateNormalizationX.w, rightNormal_andTextureCoordinateNormalizationY.w), czm_morphTime);
 
 #ifdef PER_INSTANCE_COLOR
     v_color = czm_batchTable_color(batchId);
